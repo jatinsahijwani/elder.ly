e14b4c1bf505dd5b82f78e57e823106a96799844
jatinsahijwani <117177197+jatinsahijwani@users.noreply.github.com>
2024-04-29 07:54:20 +0530

    solved issues

--- components/distance.tsx
+++ components/distance.tsx
@@ -1,39 +1,18 @@
 "use client";
 import { useState, useEffect } from "react";
-import { Howl } from 'howler';
 
 export function Distance() {
  const [value, setValue] = useState();
  const [urgent, setUrgent] = useState(false);
- const [sound, setSound] = useState<Howl | null>(null);
 
- useEffect(() => {
-    const alarmSound = new Howl({
-      src: ['/sounds/alarm.mp3'],
-      autoplay: false,
-      loop: false, 
-    });
-
-    setSound(alarmSound);
-
-    // Cleanup on component unmount
-    return () => {
-      if (alarmSound) {
-        alarmSound.unload();
-      }
-    };
- }, []);
-
+ 
  const checkDistance = async () => {
-    const response = await fetch('http://192.168.137.157/distance', {
+    const response = await fetch('http://192.168.137.28/distance', {
       method: 'GET',
     });
     const data = await response.json();
     if (data.sensorData <= 35) {
       setUrgent(true);
-      if (sound) {
-        sound.play();
-      }
     } else {
       setUrgent(false);
     }
@@ -43,7 +22,7 @@ export function Distance() {
  useEffect(() => {
     const interval = setInterval(checkDistance, 500);
     return () => clearInterval(interval);
- }, [sound]); // Add sound as a dependency to re-run effect if sound changes
+ }, []);
 
  return (
     <div className={`flex flex-col items-center justify-center h-screen ${urgent ? 'bg-red-600' : 'bg-green-500'}`}>
